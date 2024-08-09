844319b48d5c26c510cf3e7bfebe9a62de7cffa6
asif <masiff346@gmail.com>
2024-08-09 10:56:45 +0500

    update

---
+++ calcutor.py
@@ -0,0 +1,11 @@
+def add(a,d):
+    return a+b
+
+def subtract (a,d):
+    return a-b
+
+if __name__ == "__main__":
+    result =  add(2,3)
+    result_sub =  subtract(2,3)
+    print(f" the result of addition is: (result) ")
+    print(f" subtract = (result_sub): ")
